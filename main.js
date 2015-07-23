3a5a798c4aff84524a954bde38ec51eb86c30f26
Kirk Walker Graves <graves.kirk@gmail.com>
2015-07-22 21:31:36 -0500

    practiced some js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('hello from main.js');
+
+var frog = false;
+if (!frog) {
+  console.log('no frogs');
+}
+
+var age = 25;
+var female = true;
+
+if (age > 21 && female) {
+  console.log('girls night!');
+}
+
+var speed = 'fast';
+var color = 'green';
+var person = 'annoying';
+
+if (speed === 'fast' || color === 'red' && person === 'annoying') {
+  console.log('you got a ticket and are annoying');
+}
